a5a04f16c2b88424e08ea5c763158f1d67618ddc
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-06-21 19:01:21 -0300

    Create first GET and POST routes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,29 @@ import express from "express";
 
 const app = express();
 
+app.get("/users", (req, res) => {
+  const users = [
+    {
+      name: "Eduardo",
+      age: 29,
+    },
+    {
+      name: "Maria",
+      age: 20,
+    },
+    {
+      name: "Clau",
+      age: 19,
+    },
+  ];
+
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  res.send("Success");
+});
+
 app.listen(3000, () => {
   console.log("Server is running");
 });
