7274928d3db8e28c6204f09ea06531c0e1e53bee
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-06-22 22:04:58 -0300

    Create database connection

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,9 @@
+import "reflect-metadata";
 import express from "express";
 
+import "./database";
+
 const app = express();
-
-app.get("/users", (req, res) => {
-  const users = [
-    {
-      name: "Eduardo",
-      age: 29,
-    },
-    {
-      name: "Maria",
-      age: 20,
-    },
-    {
-      name: "Clau",
-      age: 19,
-    },
-  ];
-
-  res.send(users);
-});
-
-app.post("/users", (req, res) => {
-  res.send("Success");
-});
 
 app.listen(3000, () => {
   console.log("Server is running");
